3c09ccff68ad9f538ae6c8a4814e4ea772cfd2ba
SimosNeopoulos <simneo22@gmail.com>
2022-01-17 13:34:12 +0200

    Added comments and changed BufferedReader and PrintWriter object names

--- src/client/Client.java
+++ src/client/Client.java
@@ -9,15 +9,22 @@ import java.net.Socket;
 public class Client {
     public static void main(String[] args) {
         try (Socket socket = new Socket(args[0], Integer.parseInt(args[1]))) {
-            BufferedReader echoes = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter stringToEcho = new PrintWriter(socket.getOutputStream(), true);
+            /* Initiating the BufferedReader and PrintWriter objects that are going to be used to communicate with the server */
+            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+
+            /* The message to the server */
             String messageToServer = "";
+
+            /* Adding all the command line arguments to the messageToServer variable separated by a space */
             for (int i = 2; i < args.length; i++) {
                 messageToServer += args[i] + " ";
             }
-            stringToEcho.println(messageToServer);
+            /* Sending a message to the server */
+            output.println(messageToServer);
 
-            while ((messageToServer = echoes.readLine()) != null) {
+            /* Reading the answer from the server */
+            while ((messageToServer = input.readLine()) != null) {
                 System.out.println(messageToServer);
             }
 
